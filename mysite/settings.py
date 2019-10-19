84eefa3634868bae6b45aa77607215bc3db17c23
Ellie <ellie-wilkinson@hotmail.co.uk>
2019-10-19 15:03:18 +0100

    update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ah3z*!@v1gng68_@#euv*!yo6=+!3a)xdlj*%i)l%%b%hx0%bx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'elliewilk.pythonanywhere.com']
 
 
 # Application definition
